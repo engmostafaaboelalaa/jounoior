02fc20eb7a92ca15e9fb76b5b971784381a4f0e4
Mostafa Mohammed <engmostafaaboelalla@gmail.com>
2025-01-06 16:59:14 +0200

    add all new files

---
+++ student/pages/Home/scripts/custom.js
@@ -0,0 +1,30 @@
+const lists = document.querySelectorAll(".maincontainer aside .pages .links ul li.bg");
+lists.forEach(list => {
+    list.addEventListener("click", (event) => {
+        lists.forEach(item => {
+            item.classList.remove("active");
+            list.classList.add("active")
+        })
+    })
+});
+
+
+
+const buttons = document.querySelectorAll(".maincontainer .content .container .maincourses .category .btn");
+buttons.forEach(btn => {
+    btn.addEventListener("click", () => {
+        buttons.forEach(button => {
+            button.classList.remove("active");
+            btn.classList.add("active");
+        })
+    })
+});
+
+const list = document.querySelector(".maincontainer aside");
+const close = document.querySelector(".maincontainer aside .exit");
+const maincontent = document.querySelector(".maincontainer .content");
+
+close.addEventListener("click", () => {
+    list.style.display = "none";
+    maincontent.style.width = "100%";
+})
